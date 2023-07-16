2541cc6af96fa567f63321f1c75c85a96972ec32
Dima Ivanenko <dima.ivanenko.01@mail.ru>
2023-07-16 16:45:55 +0300

    Добавьте файлы проекта.

---
+++ Змейка/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Змейка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Head = new System.Windows.Forms.PictureBox();
+            this.Fruit = new System.Windows.Forms.PictureBox();
+            this.Score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Head)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fruit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 200;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Head
+            // 
+            this.Head.BackColor = System.Drawing.Color.SteelBlue;
+            this.Head.Location = new System.Drawing.Point(335, 272);
+            this.Head.Name = "Head";
+            this.Head.Size = new System.Drawing.Size(61, 64);
+            this.Head.TabIndex = 0;
+            this.Head.TabStop = false;
+            // 
+            // Fruit
+            // 
+            this.Fruit.BackColor = System.Drawing.Color.Transparent;
+            this.Fruit.Image = ((System.Drawing.Image)(resources.GetObject("Fruit.Image")));
+            this.Fruit.Location = new System.Drawing.Point(566, 179);
+            this.Fruit.Name = "Fruit";
+            this.Fruit.Size = new System.Drawing.Size(73, 75);
+            this.Fruit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Fruit.TabIndex = 1;
+            this.Fruit.TabStop = false;
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score.Location = new System.Drawing.Point(58, 53);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(122, 36);
+            this.Score.TabIndex = 2;
+            this.Score.Text = "Счет: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.Fruit);
+            this.Controls.Add(this.Head);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Head)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fruit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Head;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox Fruit;
+        private System.Windows.Forms.Label Score;
+    }
+}
+
---
+++ Змейка/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Змейка
+{
+    public partial class Form1 : Form
+    {
+        private int mapSize = 750;
+        private int indent = 50;
+        private int cellSize = 50;
+        private int score = 0;
+        private Direction direction = Direction.Right;
+        public Direction Direction { get { return direction; } 
+            set { if ((int)value != ((int)direction + 2) % 4) direction = value; } }
+
+        private List<PictureBox> snake = new List<PictureBox>();
+        public Form1()
+        {
+            InitializeComponent();
+            this.ClientSize = new Size(mapSize + 2 * indent, mapSize + 2 * indent);
+
+            Head.Size = new Size(cellSize, cellSize);
+            Head.Location = new Point(indent + 3 * cellSize, (indent + mapSize) / 2);
+            GenerateNewFruit();
+
+            snake.Add(Head);
+            Score.Location = new Point(indent, 10);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.TranslateTransform(indent, indent);
+            Pen pen = new Pen(Color.Black, 2);
+            for (int i = 0; i <= mapSize; i += cellSize)
+            {
+                g.DrawLine(pen, new Point(0, i), new Point(mapSize, i));
+            }
+            for (int i = 0; i <= mapSize; i += cellSize)
+            {
+                g.DrawLine(pen, new Point(i, 0), new Point(i, mapSize));
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    Direction = Direction.Left;
+                    break;
+                case Keys.Up:
+                    Direction = Direction.Up;
+                    break;
+                case Keys.Right:
+                    Direction = Direction.Right;
+                    break;
+                case Keys.Down:
+                    Direction = Direction.Down;
+                    break;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            MoveForward();
+            CheckForLose();
+            EatFruit();
+        }
+
+        private void CheckForLose()
+        {
+            if (IsOverBorders() || IsEatItself())
+            {
+                timer.Stop();
+                MessageBox.Show("GAME OVER");
+            } 
+        }
+
+        private void EatFruit()
+        {
+            if (Head.Bounds.IntersectsWith(Fruit.Bounds))
+            {
+                Grow();
+                GenerateNewFruit();
+                Score.Text = $"Счет: {++score}";
+            }
+        }
+
+        private void GenerateNewFruit()
+        {
+            int x, y;
+            Random r = new Random();
+            do
+            {
+                x = r.Next(indent, mapSize);
+                x -= x % cellSize;
+                y = r.Next(indent, mapSize);
+                y -= y % cellSize;
+            } while (IsIntersects(new Point(x, y)));
+            int fruitIndent = indent / 4;
+            Fruit.Size = new Size(cellSize - fruitIndent, cellSize - fruitIndent);
+            Fruit.Location = new Point(fruitIndent / 2 + x, fruitIndent / 2 + y);
+        }
+
+        private bool IsIntersects(Point point)
+        {
+            foreach (PictureBox node in snake)
+            {
+                if (node.Bounds.IntersectsWith(new Rectangle(point, new Size(cellSize, cellSize)))) return true;
+            }
+            return false;
+        }
+
+        private bool IsOverBorders()
+        {
+            if (Head.Left >= indent && Head.Right <= mapSize + indent && Head.Top >= indent && Head.Bottom <= mapSize + indent) return false;
+            return true;
+        }
+
+        private bool IsEatItself()
+        {
+            for (int i = 1; i < snake.Count; i++)
+            {
+                if (snake[i].Bounds.IntersectsWith(Head.Bounds)) return true;
+            }
+            return false;
+        }
+
+        private void Grow()
+        {
+            PictureBox node = new PictureBox();
+            node.Size = Head.Size;
+            node.BackColor = Color.DarkBlue;
+            node.Location = Head.Location;
+            Controls.Add(node);
+            snake.Add(node);
+        }
+
+        private void MoveLeft()
+        {
+            Head.Left -= cellSize;
+        }
+
+        private void MoveUp()
+        {
+            Head.Top -= cellSize;
+        }
+
+        private void MoveRight()
+        {
+            Head.Left += cellSize;
+        }
+
+        private void MoveDown()
+        {
+            Head.Top += cellSize;
+        }
+
+        private void MoveForward()
+        {
+            for (int i = snake.Count - 1; i > 0; i--)
+            {
+                snake[i].Location = snake[i - 1].Location;
+            }
+            switch (direction)
+            {
+                case Direction.Up:
+                    MoveUp();
+                    break;
+                case Direction.Down:
+                    MoveDown();
+                    break;
+                case Direction.Right:
+                    MoveRight();
+                    break;
+                case Direction.Left:
+                    MoveLeft();
+                    break;
+            }
+        }
+    }
+}
+
+public enum Direction
+{
+    Left,
+    Up,
+    Right,
+    Down
+}
